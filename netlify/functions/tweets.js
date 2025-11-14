85dcc752f4bd49d8e31b482bb42975462cb226fd
Alex Barbulescu <alex_tnt89@yahoo.com>
2025-11-14 11:00:34 +0200

    Remove tweets API endpoint handlers
    Deleted the minimal handlers for the tweets API in both the api and Netlify functions directories, fully disabling the endpoint.

--- netlify/functions/tweets.js
+++
@@ -1,7 +0,0 @@
-// Tweets endpoint disabled. Keep minimal handler to avoid 404s if deployed.
-exports.handler = async function(event) {
-  if (event.httpMethod === 'OPTIONS') {
-    return { statusCode: 204, headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, OPTIONS', 'Access-Control-Allow-Headers': 'Content-Type' } }
-  }
-  return { statusCode: 200, headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }, body: JSON.stringify({ items: [] }) }
-}
