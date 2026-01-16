13d34bbc39adc73de640080e805da7e1d2f44574
Alex Barbulescu <alex_tnt89@yahoo.com>
2026-01-16 14:37:46 +0200

    Add Cloudflare adapter and Vite config for SvelteKit
    Switched SvelteKit adapter from auto to Cloudflare, added Vite configuration file, and updated .gitignore for Cloudflare Wrangler. These changes prepare the project for deployment on Cloudflare Workers.

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	server: {
+		port: 3000,
+		host: '127.0.0.1',
+		strictPort: true
+	},
+	preview: {
+		port: 3000,
+		host: '127.0.0.1',
+		strictPort: true
+	},
+	build: {
+		sourcemap: true
+	}
+});
